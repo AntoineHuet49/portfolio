edf5261b802d7fc9edffd3e294960bba42a2f07a
likin <antoine.huet.webdev@gmail.com>
2023-03-31 18:30:57 +0200

    card ok

--- src/components/Accueil/Card/index.js
+++ src/components/Accueil/Card/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.css";
+import imageTodoList from "./../../../utils/images/fondEcranMontagne.jpg"
 
 class Card extends React.Component {
     // state
@@ -11,9 +12,7 @@ class Card extends React.Component {
         return (
             <div className='card'>
                 <h3 className='card__title'>TodoList</h3>
-                <div className='card__img'>
-                    <img src='' alt=''></img>
-                </div>
+                    <img className='card__img' src={imageTodoList} alt=''></img>
                 <p className='card__description'>Une TodoList développé avec Symfony et React</p>
             </div>
         )
