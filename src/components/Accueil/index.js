09c0230cde275d695eccc81afc08a7a3e6f0a27f
likin <antoine.huet.webdev@gmail.com>
2023-03-31 09:32:13 +0200

    Creation app

---
+++ src/components/Accueil/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './index.css';
+
+function Accueil() {
+    // state
+
+    // function
+
+    //render
+    return (
+        <div className="container">
+            
+        </div>
+    );
+}
+
+export default Accueil;
