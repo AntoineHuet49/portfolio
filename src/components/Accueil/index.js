d521056294eda3d1322b61b4cd033fb584cd3d16
likin <antoine.huet.webdev@gmail.com>
2023-03-31 14:10:33 +0200

    présentation ok

--- src/components/Accueil/index.js
+++ src/components/Accueil/index.js
@@ -30,6 +30,13 @@ class Accueil extends React.Component {
                     <h1 className="mainTitle">Huet Antoine</h1>
                     <span className='title__barre'></span>
                     <h2 className="secondTitle">Développeur web et web mobile</h2>
+
+                    <main className='main'>
+                        <div className='main__presentation'>
+                            <h3>Bienvenue !</h3>
+                            <p>Je me présente, je suis un développeur agé de 27ans spécialisé dans le back-end, surtout sur Symfony. Je sais néanmoins faire du front-end égualement comme le montre ce site codé grâce au Framework React. Sur ce site vous pouvez retrouver mes different projet perso et en entreprise, afin de voir ce que je suis capable de faire. Vous pourrez télécharger mon CV si vous le souhaitez et me contecter par mail. Bonne visite !</p>
+                        </div>
+                    </main>
                 </div>
             </div>
         );
