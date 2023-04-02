44e088f200ec2662096905fe72a125b0e0a7fb60
likin <antoine.huet.webdev@gmail.com>
2023-04-02 10:46:24 +0200

    respoonsive ok

--- src/components/Accueil/index.js
+++ src/components/Accueil/index.js
@@ -10,7 +10,8 @@ class Accueil extends React.Component {
         this.state = {
             //TODO remettre comme il faut presentation sur true
             presentation: false,
-            projet: true,
+            projet: false,
+            contact: true,
             cards: []
         }
     }
@@ -20,20 +21,32 @@ class Accueil extends React.Component {
         this.handleMenuBtnClick();
         this.setState({presentation: true});
         this.setState({projet: false});
+        this.setState({contact: false});
     }
 
     handleClickProjet = (e) => {
         this.handleMenuBtnClick();
         this.setState({presentation: false});
         this.setState({projet: true});
+        this.setState({contact: false});
+    }
+
+    handleClickContact = (e) => {
+        this.handleMenuBtnClick();
+        this.setState({presentation: false});
+        this.setState({projet: false});
+        this.setState({contact: true});
     }
 
     handleMenuBtnClick = (e) => {
         const menuBtn = document.querySelector(".menu");
         menuBtn.classList.toggle('opened');
 
-        const container = document.querySelector(".background");
-        container.classList.toggle("nav--blur")
+        const background = document.querySelector(".background");
+        background.classList.toggle("nav--blur")
+
+        const content = document.querySelector('.content');
+        content.classList.toggle("nav--blur");
 
         this.toggleNav();
     }
@@ -48,11 +61,15 @@ class Accueil extends React.Component {
 
         return (
             <div className="container">
-                <Nav handleClickAccueil={this.handleClickAccueil}
-                    handleClickProjet={this.handleClickProjet}
-                    handleMenuBtnClick={this.handleMenuBtnClick}
-                />
-                <div className='background'>
+                <header>
+
+                    <Nav handleClickAccueil={this.handleClickAccueil}
+                        handleClickProjet={this.handleClickProjet}
+                        handleClickContact={this.handleClickContact}
+                        handleMenuBtnClick={this.handleMenuBtnClick}
+                    />
+                </header>
+                <div className='content'>
                     <h1 className="mainTitle">Huet Antoine</h1>
                     <span className='title__barre'></span>
                     <h2 className="secondTitle">Développeur web et web mobile</h2>
@@ -71,8 +88,26 @@ class Accueil extends React.Component {
                         </div> 
                         : null
                     }
+                    {this.state.contact ?
+                        <div className='main__contact'>
+                            <form id='contact__form'>
+                                <label for="name">Nom</label>
+                                <input name="name"></input>
+                                <label for="email">Email</label>
+                                <input name="email" type='email'></input>
+                                <label for="message">Message</label>
+                                <textarea name='message' type="text"></textarea>
+                                <button></button>
+                            </form>
+                        </div>
+                        : null
+                    }
                     </main>
                 </div>
+                <div className='background'></div>
+                <footer className='footer'>
+
+                </footer>
             </div>
         );
     }
