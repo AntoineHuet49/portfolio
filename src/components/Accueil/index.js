363db05c614ccfd712d4df840fcae354e636a060
likin <antoine.huet.webdev@gmail.com>
2023-04-03 18:23:31 +0200

    faute d'ortographe

--- src/components/Accueil/index.js
+++ src/components/Accueil/index.js
@@ -105,7 +105,7 @@ class Accueil extends React.Component {
                     {this.state.presentation ? 
                         <div className='main__presentation'>
                             <h3>Bienvenue !</h3>
-                            <p>Jeune développeur de 27 ans, je me suis spécialisé dans le back-end, surtout sur Symfony. Je sais également coder du front-end, notamment avec le Framework React, comme le montre ce site. Ici, vous pouvez retrouver mes différents projets perso et en entreprise, afin de voir ce que je suis capable de faire. Vous pourrez télécharger mon CV si vous le souhaitez et me contacter par mail. Bonne visite ! </p>
+                            <p>Jeune développeur de 27 ans, je me suis spécialisé dans le back-end, surtout sur Symfony. Je sais également coder du front-end, notamment avec le Framework React, comme le montre ce site. Ici, vous pouvez retrouver mes différents projets perso et d'entreprise, réalisations montrant ce que je suis capable de faire. Vous pouvez aussi télécharger mon CV et me contacter par mail. Bonne visite !</p>
                         </div> 
                         : null
                     }
@@ -118,7 +118,7 @@ class Accueil extends React.Component {
                     {this.state.contact ?
                         <div className='main__contact'>
                             <form onSubmit={this.handleSubmitContactForm} id='contact__form'>
-                                <h3>Envoyé moi un mail!</h3>
+                                <h3>Envoyez moi un mail!</h3>
                                 <label autoFocus for="name">Nom</label>
                                 <input name="name"></input>
                                 <label for="email">Email</label>
